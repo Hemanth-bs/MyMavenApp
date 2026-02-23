7870259daceaf35e4a6bb0b47a8b618c0b158e3f
Hemanth-bs <hemanthbyrareddy@gmail.com>
2026-02-23 09:45:10 +0530

    add mainclass

---
+++ src/main/java/com/example/mainclass.java
@@ -0,0 +1,13 @@
+package com.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class mainclass
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "this is main class" );
+    }
+}
